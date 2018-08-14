9d208ff4748c017891eb8d23ed55a4fde4107e1f
Gloria Budiman <wahyu.g@gmail.com>
2018-08-13 22:11:44 -0700

    Prototype Stat class

---
+++ ProjectViolet3/Assets/Scripts/Stats/ActorStat.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorStat : Stat {
+  public int str;
+  public int agi;
+  public int vit;
+  public int itl;
+  public int dex;
+  public int fai;
+
+  public enum Stat { str, agi, vit, itl, dex, fai };
+
+  ActorStat(int _hp, int _max_hp, int _str, int _agi, int _vit, int _itl, int _dex, int _dex, int _fai) {
+    base.hp = _hp;
+    base.max_hp = _max_hp;
+    str = _str;
+    agi = _agi;
+    vit = _vit;
+    itl = _itl;
+    dex = _dex;
+    fai = _fai;
+  }
+
+  public void SetStat(Stat _stat, int x) {
+
+  }
+
+  public void AdjustStat(Stat _stat, int x) {
+
+  }
+}
---
+++ ProjectViolet3/Assets/Scripts/Stats/Stat.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stat {
+  public int hp;
+  public int max_hp;
+  public float mass;
+  
+  Stat(int _hp) {
+    SetMaxHp(_hp);
+    SetHp(_hp);
+  }
+
+  Stat(int _hp, int _hp_remaining) {
+    SetMaxHp(_hp);
+    SetHp(_hp);
+  }
+
+  public void AdjustHP(int x) {
+
+  }
+
+  public void SetHP(int x) {
+
+  }
+
+  public void AdjustMaxHP(int x) {
+
+  }
+
+  public void SetMass(float _mass) {
+
+  }
+
+  public void AdjustMass(float _mass) {
+
+  }
+
+  public void SetMass(float _mass) {
+    mass = _mass;
+  }
+
+  protected void SetMaxHp(int _max_hp) {
+    max_hp = _max_hp;
+  }
+
+  protected void SetHp(int _hp) {
+    hp = _hp;
+  }
+}
