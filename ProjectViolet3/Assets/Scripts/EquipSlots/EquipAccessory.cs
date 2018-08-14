62cf918178e5331a94f895fac4ba64d5d4a3befc
Gloria Budiman <wahyu.g@gmail.com>
2018-08-13 22:35:18 -0700

    Add Equipment prototypes

---
+++ ProjectViolet3/Assets/Scripts/EquipSlots/EquipAccessory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipAccessory : EquipSlot {
+  string granted_by;
+
+  EquipAccessory(string _name, string _granted_by): base(_name) {
+    granted_by = _granted_by;
+  }
+
+  public bool IsRemovedBy(string _conditional_name) {
+    return base.name == _conditional_name
+  }
+}
---
+++ ProjectViolet3/Assets/Scripts/EquipSlots/EquipSlot.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class EquipSlot {
+  int armor_point;
+  string name;
+
+  EquipSlot(string _name) {
+    name = _name;
+  }
+}
