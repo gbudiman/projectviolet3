cde89c31eff503f1db9a5caae012ad53c24e5cab
Gloria Budiman <wahyu.g@gmail.com>
2018-08-14 22:12:03 -0700

    Add Probabilistic Methods prototype

---
+++ ProjectViolet3/Assets/Scripts/TacticalInteraction/ProbabilisticMechanic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbabilisticMechanic {
+  /// <summary>
+  /// Roll dice in uniform probability distribution
+  /// </summary>
+  /// <returns>The roll.</returns>
+  /// <param name="min">Minimum.</param>
+  /// <param name="max">Max.</param>
+  public static bool Roll(float min, float max) {
+    return false;
+  }
+
+  /// <summary>
+  /// Roll dice in gaussian probability distribution
+  /// </summary>
+  /// <returns>The roll.</returns>
+  /// <param name="min">Minimum.</param>
+  /// <param name="max">Max.</param>
+  /// <param name="mu">Mu.</param>
+  /// <param name="sigma">Sigma.</param>
+  public static bool Roll(float min, float max, float mu, float sigma) {
+    return false;
+  }
+
+  /// <summary>
+  /// Add jitter 
+  /// </summary>
+  /// <returns>The jitter.</returns>
+  /// <param name="mu">Mu.</param>
+  /// <param name="sigma">Sigma.</param>
+  public static float Jitter(float mu, float sigma) {
+    return 0;
+  }
+}
